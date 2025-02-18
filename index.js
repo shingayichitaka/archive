27ba0a6d5717114de45f53200d777094705402ba
shingayichitaka <shingayidambudzochitaka@gmail.com>
2025-02-18 13:38:07 +0100

    added user interface cli for accepting projects via the cli

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+import readline from "readline";
+import { addProject } from "./utils.js";
+
 const greet = function (name) {
   return `Hello ${name}!`;
 };
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What project have you completed? ", (projectName) => {
+  console.log(`Project ${projectName} --- Completed ✅`);
+  addProject(projectName);
+  rl.close();
+});
 
 export { greet };
